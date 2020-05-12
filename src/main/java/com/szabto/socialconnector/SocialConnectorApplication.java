a24eb5d54914b9ee6e6c650ec98d529ec0f87124
Szabó Tamás <szabto@gmail.com>
2020-05-12 17:47:33 +0200

    Spring initializer

---
+++ src/main/java/com/szabto/socialconnector/SocialConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.szabto.socialconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szabto/socialconnector/SocialConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szabto.socialconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
